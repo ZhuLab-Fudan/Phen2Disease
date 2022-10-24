9282b09f26b6dcb0e07cdded29354b859181792d
BruceZwq <20110850010@fudan.edu.cn>
2022-10-24 10:14:34 +0800

    leafnode filter

--- src/model/Phen2Disease/Phen2Disease-patient.py
+++ src/model/Phen2Disease/Phen2Disease-patient.py
@@ -220,7 +220,7 @@ for file in files_patient_folder:
             if term in inheritance_list:
                 judgment = judgment + 1
             term_disease_filter_temp_list = []
-            for term_d in ontology_t1.get_descendants(term):
+            for term_d in ontology_t1.get_descendants([term]):
                 term_disease_filter_temp_list.append(term_d)
             for term_f in disease_term_list:
                 if term_f in term_disease_filter_temp_list:
@@ -236,7 +236,7 @@ for file in files_patient_folder:
             if term in inheritance_list:
                 judgment = judgment + 1
             term_patient_filter_temp_list = []
-            for term_d in ontology_t1.get_descendants(term):
+            for term_d in ontology_t1.get_descendants([term]):
                 term_patient_filter_temp_list.append(term_d)
             for term_f in patient_term_list:
                 if term_f in term_patient_filter_temp_list:
